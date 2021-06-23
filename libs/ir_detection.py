f810cf8a24bc67fdeedf51111a4a00210a5928ce
PaulaScharf <paulascharf@gmx.de>
2021-06-23 23:17:05 +0200

    make IR drawing pink

--- libs/ir_detection.py
+++ libs/ir_detection.py
@@ -9,6 +9,6 @@ def detect(irframe, colorframe):
     # ret, thresholded = cv2.threshold(blurred, 50, 255, 0)  # TODO: VERY BASIC, TRY OTHER THRESHHOLDS
     ret, thresholded = cv2.threshold(blurred, 200, 255, cv2.THRESH_BINARY)
     # th3 = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-    tempOut = np.zeros_like(irframe)  # Extract out the object and place into output image
-    tempOut[thresholded == 255] = irframe[thresholded == 255]
+    tempOut = np.zeros_like(colorframe)  # Extract out the object and place into output image
+    tempOut[thresholded == 255] = [202,3,252]
     return tempOut
